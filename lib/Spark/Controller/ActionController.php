ed30ab14a60adcc1a7f09cf33fe599cdbabc7a6f
Christoph Hochstrasser <christoph.hochstrasser@googlemail.com>
2010-05-09 20:51:15 +0200

    Fixed ActionController

--- lib/Spark/Controller/ActionController.php
+++ lib/Spark/Controller/ActionController.php
@@ -1,25 +1,29 @@
 <?php
 
-class Spark_Controller_ActionController implements Spark_Controller_CommandInterface
+abstract class Spark_Controller_ActionController implements Spark_Controller_CommandInterface
 {
   public function execute(
     Spark_Controller_RequestInterface $request,
     Zend_Controller_Response_Abstract $response
   )
   {
-    $action = $request->getAction();
+    $action = $request->getActionName();
     
     if($action == null) {
       $action = "index";
     }
     
-    $method = str_replace(" ", "", ucwords(str_replace("_", " ", strtolower($action))));
+    $method = str_replace(" ", "", ucwords(str_replace("_", " ", str_replace("-", " ", strtolower($action)))));
     $method[0] = strtolower($method[0]);
     
     $method = $action . "Action";
     
     if(method_exists($this, $method)) {
-      $this->$method();
+      $this->$method($request, $response);
+    } else {
+      $controller = get_class($this);
+      throw new Spark_Controller_Exception("The action {$action} was not found in
+        the controller {$controller}. Please make sure the method {$method} exists.", 404);
     }
     
   }
