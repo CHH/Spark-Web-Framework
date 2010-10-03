818e00596dd2c71b02724f56903661772286fed9
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2010-10-03 10:22:51 +0200

    some fixes

--- lib/Spark/Relation/Relation.php
+++ lib/Spark/Relation/Relation.php
@@ -1,11 +1,27 @@
 <?php
 
-interface Spark_Relation_Relation extends Iterator
+interface Spark_Relation_Relation
 {
-    public function project($projection = array());
-    public function create(array $row);
-    public function update(array $row, $select = null);
-    public function delete($select = null);
-    
+    /**
+     * Returns the Name of the relation, used to create queries
+     *
+     * @return string
+     */
     public function getName();
+    
+    /**
+     * Creates a projection of the relation
+     *
+     * @return Spark_Relation_Query
+     */
+    public function project($projection = array());
+    
+    /**
+     * Creates a tupel
+     *
+     * @param array $row
+     */
+    public function create(Array $row);
+    public function update(Array $row, $select = null);
+    public function delete($select = null);
 }
